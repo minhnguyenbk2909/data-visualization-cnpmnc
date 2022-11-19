448fdd004566b5a26bce099e4fc1a07d86d15b21
minhnguyenbk2909 <minh.nguyen.quang2909@hcmut.edu.vn>
2022-11-19 14:02:53 +0700

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,23 +8,29 @@ router.get("/data/daily", async (req, res) => {
   let month = req.query.month;
   let year = req.query.year;
   let date = `${month}-${day}-${year}`;
-  console.log(date);
-  res.send(await ctrl.getByDate(date));
+  let country = req.query.country;
+  let data = await ctrl.getByDate(date);
+  if (!country) {
+    res.send(data);
+  } else {
+    res.send(data.find((x) => x.Country_Region == country));
+  }
 });
 
 router.get("/data/filter", async (req, res) => {
+  // W.I.P
   let from = req.query.from;
   let to = req.query.to;
   let country = req.query.country;
   res.send(`Filter from ${from} to ${to} of ${country}`);
 });
 
-router.get('/country-names', (req, res, next) => {
+router.get("/country-names", (req, res, next) => {
   const responseData = {
     statusCode: 0,
-    countryNames: countryNames
-  }
+    countryNames: countryNames,
+  };
   res.status(200).send(JSON.stringify(responseData));
-})
+});
 
 module.exports = router;
