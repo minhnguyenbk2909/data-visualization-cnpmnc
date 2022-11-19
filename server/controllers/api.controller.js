2703a0568c20eb496d42a28ceb3a73fb58fe8655
minhnguyenbk2909 <minh.nguyen.quang2909@hcmut.edu.vn>
2022-11-19 10:17:24 +0700

    Added controller, csv-parser

---
+++ server/controllers/api.controller.js
@@ -0,0 +1,7 @@
+const csv = require("../data/11-17-2022.csv");
+
+class ApiController {}
+
+const ac = new ApiController();
+
+module.exports = ac;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
 
+const ctrl = require("../controllers/api.controller");
+
+router.get("/countries/:country", (req, res) => {
+  res.send(ctrl.getCountry(req.params.country));
+});
+
 module.exports = router;
