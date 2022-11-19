5f3aa75ebdb5df0f817ff02c40824a627151f73f
hoang-cs <hoangproyume@gmail.com>
2022-11-19 13:45:06 +0700

    add api get country names

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const countryNames = require("../constant/countries");
 
 const ctrl = require("../controllers/api.controller");
 
@@ -8,6 +9,14 @@ router.get("/countries/:country", (req, res) => {
 
 router.get("/data/daily/:date", async (req, res) => {
   res.send(await ctrl.getByDate(req.params.date));
+})
+
+router.get('/country-names', (req, res, next) => {
+  const responseData = {
+    statusCode: 0,
+    countryNames: countryNames
+  }
+  res.status(200).send(JSON.stringify(responseData));
 })
 
 module.exports = router;
